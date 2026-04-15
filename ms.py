c0a6628493de665c3a5059bbf57c5f1fe405ba83
Flussziege <flusszieger@gmail.com>
2026-04-15 16:00:30 +0200

    hhhhh

--- ms.py
+++ ms.py
@@ -569,8 +569,8 @@ def _generate_cleavage_peaks(mol: Chem.Mol, rng: random.Random) -> List[Peak]:
                 if label == "bond cleavage":
                     label = "aromatic-stabilized fragment"
                 
-                if _is_benzene_like(fragment):
-                    continue
+            if _is_benzene_like(fragment):
+                 continue
 
             intensity = _jitter(intensity, rng, 0.9, 1.1)
 
