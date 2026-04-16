4e131a102cefc005320d19adac5326c1efb6f8c3
Flussziege <flusszieger@gmail.com>
2026-04-16 18:20:00 +0200

    sfdg

--- IR_banden.py
+++ IR_banden.py
@@ -826,7 +826,7 @@ FUNCTIONAL_GROUPS.update({
             (1180, 0.14, 18,  "Ar-O str II"),
             (1150, 0.10, 18,  "phenolic deformation"),
             (980,  0.08, 18,  "OH wag"),
-            (750,  0.04, 20,  "OH oop phenol"),
+            (750,  0.01, 20,  "OH oop phenol"),
             (690,  0.10, 16,  "Ar-O deformation"),
         ]
     },
@@ -1205,7 +1205,7 @@ AROMATIC_CORE = {
         (900,  0.08, 16, "ArH oop"),
         (860,  0.08, 16, "ArH oop"),
         (820,  0.08, 16, "ArH oop"),
-        (760,  0.10, 14, "ArH oop"),
+        (760,  0.08, 14, "ArH oop"),
         (690,  0.75, 14, "ArH oop"),
     ]
 }
